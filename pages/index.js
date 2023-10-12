4f86a92cf1963cd299c1e7f3439161d1c886664a
Paul_Killian <psk65lava@gmail.com>
2023-10-12 15:33:24 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -61,8 +61,8 @@ const Scroll = () => {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-          <div>
-            <div className="full-width-image-container overflow-y-hidden">
+          <div className='overflow-x-hidden'>
+            <div className="full-width-image-container">
               <div >
                 <Image
                   src="/helm2.png" // Path to your image in the public folder
