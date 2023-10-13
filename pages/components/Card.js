47faa306f03582151027c6aa15bd3e06bd02ed93
Paul_Killian <psk65lava@gmail.com>
2023-10-13 11:55:56 -0700

    image scale

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -3,16 +3,19 @@ import {Button} from "@nextui-org/react";
 
 const Card = ({name, img, url}) => {
     return (
-        <div class="max-w-sm rounded overflow-hidden shadow-md bg-stone-100">
+        <div class="container max-w-sm rounded overflow-hidden shadow-md out-tr bg-stone-100 container">
         <Image 
+          className='image'
           src={img} 
-          alt="Project Image" />
+          alt="Project Image" 
+        />
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">{name}</div>
         </div>
-        <button className='text-stone-50 bg-indigo-400 p-2 ml-5 mb-2 rounded-md' color="primary" variant="ghost">
+        <button className='text-stone-50 bg-indigo-400 p-2 ml-5 mb-2 rounded-md'>
           <a href={url}>Check It Out</a>
-        </button>  
+        </button> 
+        <div className="pop"></div> 
       </div>
     )
   } 
